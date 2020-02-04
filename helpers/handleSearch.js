db342df216e1ee09e8ee74bd144bc533e6bda2ab
Lasse <lasse.tammela@aalto.fi>
2020-02-04 19:37:18 +0200

    added async middleware to handle failed promises

--- helpers/handleSearch.js
+++ helpers/handleSearch.js
@@ -10,7 +10,7 @@ Checks distance to the lat-lon pair given in the request.
 Checks restaurant's name and tags and compares them to the search query. 
 */
 function handleSearch(req) {
-    const restaurants = getDictValues()
+    const restaurants = getDictValues();
     const {q,lat,lon} = req.query;
     //test wheter both lat and lon were given
     const location = lat && lon ? [lat,lon] : false
@@ -21,16 +21,16 @@ function handleSearch(req) {
     const reqExp = new RegExp(filter,'i')
 
     const filteredRestaurants = restaurants.filter(obj => {
-        let retVal = false
+        let retVal = false;
         if(testIfObjectContains(reqExp,obj)) {
             //if no location is given we allow all restaurants whose name matches the search query pass the filter.
             if(!location) {
-                retVal = true
+                retVal = true;
             //if location is given we only return restaurants close by.
             } else if (testLocation(location,obj)) {
-                retVal = true
+                retVal = true;
             } else {
-                retVal = false
+                retVal = false;
             }
         } 
         //retVal is going to be false for restaurants whose name nor tags match the search query.
@@ -41,8 +41,8 @@ function handleSearch(req) {
 
 // Checks if restauant's location and client's location is under 3 km
 function testLocation(location,obj) {
-    const distance = calculateDistance(location,obj.location)
-    return distance <= 3
+    const distance = calculateDistance(location,obj.location);
+    return distance <= 3;
 }
 
 // Checks if restaurants name matches search query
@@ -52,4 +52,4 @@ function testIfObjectContains(reqExp,obj){
 
 
 
-module.exports = handleSearch
+module.exports = handleSearch;
